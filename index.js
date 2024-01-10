3c537494beaa03546022a07260cff9382cca166f
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-01-10 17:46:07 -0500

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const tutorialsTitled = tutorials.map(tutorial => {
+    tutorial = tutorial[0].toUpperCase() + tutorial.substring(1);
+    for (let i = 1; i < tutorial.length; i++) {//i think maybe you can use "for of" if you specify arraylike or something
+      if (tutorial[i-1] === ' ' && i != tutorial.length - 1) {
+        //console.log(tutorial.substring(0,i) + tutorial[i].toUpperCase() + tutorial.substring(i + 1))
+        tutorial = tutorial.substring(0,i) + tutorial[i].toUpperCase() + tutorial.substring(i + 1);
+      }
+    }
+    return tutorial;
+  })
+  return tutorialsTitled;
 }
+
+console.log(titleCased())
